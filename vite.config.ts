7ce221745758f4c2e9e4fec95d9b1f4bf53eef45
André Pettersson <andre20030417@gmail.com>
2025-12-09 13:45:12 +0100

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // Konfiguration som hanterar både lokal utveckling och GitHub Pages
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // Använd '/' lokalt, men '/Brogarden2/' när vi bygger för produktion
-  base: mode === 'production' ? '/Brogarden2/' : '/',
+  // Använd '/' lokalt, men '/Brogarden/' när vi bygger för produktion
+  base: mode === 'production' ? '/Brogarden/' : '/',
   build: {
     outDir: 'dist',
   },
